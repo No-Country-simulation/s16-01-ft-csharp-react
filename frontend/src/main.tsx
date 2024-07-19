5d52a678c4b8f9067ed6b3cf65f9230bec29d8af
Rogelio Olarte <galaxino12@gmail.com>
2024-07-19 02:50:22 -0500

    created various components, pages and routes
    First components and pages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from '@material-tailwind/react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import routes from './config/routes.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter(routes)
+
+const root = createRoot(document.getElementById('root')!)
+root.render(
+  <ThemeProvider>
+    {/* <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider> */}
+    <RouterProvider router={router} />
+  </ThemeProvider>
+)
+
+/* ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+) */
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,6 +9,9 @@ export default withMT({
   theme: {
     extend: {},
   },
+  fontFamily: {
+    sans: ['Roboto']
+  },
   plugins: [],
 })
 
