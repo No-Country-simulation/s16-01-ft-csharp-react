079f1ed97a6dacebed2079dc46228d9ac1751ac1
Rogelio Olarte <galaxino12@gmail.com>
2024-07-19 11:17:30 -0500

    created loadingScreen

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,5 +1,19 @@
+import { Button } from "@material-tailwind/react";
+import { Link } from "react-router-dom";
+import { useUserActions } from '../hooks/useUserActions'
+
 export default function HomePage() {
+  const { user, useSetUser } = useUserActions();
+  const handleOpenWithContext = () => {
+    useSetUser({ ...user, user_state: 'Loading' })
+  }
+
   return (
-    <div>HomePage</div>
+    <div>
+      HomePage
+      <Link to="/register">Register</Link>
+      <Link to="/preference">Register</Link>
+      <Button onClick={handleOpenWithContext}>Open Modal</Button>
+    </div>
   )
 }
